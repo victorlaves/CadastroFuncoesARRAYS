38032aa242a234d2b406dbc72f5fea8ae5a5286c
Victor Laves <victorpf2020@gmail.com>
2022-11-08 22:33:33 -0300

    Fluxo de funcionamento

--- CadastroDoWhile.js
+++ CadastroDoWhile.js
@@ -1,30 +1,43 @@
 let nomes = [], senhas = []
 let continuar = true
+let usuarioV
 
 function CadastroNomeESenha(){
     nomes.push(prompt("Digite seu nome"))
     senhas.push(prompt("Digite sua senha"))
-    return nome, senha
+    console.log(nomes, senhas)
 }
 
 function Opcoes(){
     let opcao = prompt("Digite 1 para Cadastrar | 2 Para Login | 3 Para Excluir usuario | 4 Encerrar programa")
+  do{
     switch(opcao) {	
-    case 1:
+    case "1":
         CadastroNomeESenha()
-        reak;  
-    case 2:
-        Login()
+        Opcoes()
+        break;  
+    case "2":
+        let nome = prompt("Digite o nome")
+        let senha = prompt("Digite senha")
+        if(Login(nome,senha))
+        console.log("Login feito com Sucesso")
+        else
+        console.log("Nome ou senha incorretos")
+        Opcoes()
         break;
-    case 3:
-        Excluir()
+    case "3":
+        let nomeExcluido = prompt("Digite O Nome para excluir")
+        Excluir(nomeExcluido)
+        Opcoes()
         break;
-    case 4:
+    case "4":
         continuar = false
         break;
     default:
     console.log("Opção invalida")
+    break;
     }
+} while(continuar == true)
 }
 
 function Login(nome,senha) {
@@ -41,4 +54,11 @@ function Login(nome,senha) {
         else 
         return false
 }
-        
+
+    function Excluir(nomeExcluido){
+        usuarioV = nomes.indexOf(nomeExcluido)
+        nomes.splice(usuarioV, 1)
+        senhas.splice(usuarioV, 1)
+    }
+
+Opcoes()
